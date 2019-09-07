e29580b6d256bf29e91d49c95f52d2d704e399fa
KailerS <KailerS@users.noreply.github.com>
2019-09-07 13:29:16 -0600

    Working on js added some arrays

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Word-Guess-Game
+array string split link on slack .length
+var game = {
+    wordBank: ["trout", "bellinger", "yelich", "pujols", "haniger", "goldschmidt", "molina"],
+    wins: 0,
+    remainingGuesses: 12,
+    wordGenerator : function() {
+        return this.wordBank[Math.floor(Math.random() * this.wordBank.length)];
+         
+    },
+    winsIncrements: function(){
+        this.wins++
+    } 
+}
+// console.log(game.wordGenerator());
+var word = game.wordGenerator()
+console.log(word)
+console.log(game.wins)
--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,1 +1,26 @@
-var wordBank = ["trout", "bellinger", "yelich", "pujols", "haniger", "goldschmidt", "molina"]
+var wordBank = ["trout", "bellinger", "yelich", "pujols", "haniger", "goldschmidt", "molina"];
+var workingCharacters = ["a","b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
+var currentWord = ["-","-"]
+var wins = 0;
+var remainingGuesses = 12;
+var wordGenerator = wordBank[Math.floor(Math.random() * wordBank.length)];
+var letter = wordGenerator[0]
+
+
+document.onkeyup = function(event){  
+
+    var userGuess = event.key;
+
+    var wordGenerator = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+    if (workingCharacters.indexOf(userGuess) === -1 ){
+       return alert("Enter a valid character")   
+    }
+
+    
+
+
+}
+
+console.log(wordGenerator);
+console.log(letter)
