ab7f2bd323ee7048fcd7e40323b93af31a902afa
KailerS <KailerS@users.noreply.github.com>
2019-09-07 14:48:54 -0600

    small changes to html and css. Added text links in js

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,26 +1,55 @@
 var wordBank = ["trout", "bellinger", "yelich", "pujols", "haniger", "goldschmidt", "molina"];
-var workingCharacters = ["a","b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
-var currentWord = ["-","-"]
+var workingCharacters = ["a","b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+var wordPicked = wordBank[Math.floor(Math.random() * wordBank.length)];
+var answer = [];
+for (var i = 0; i < wordPicked.length; i++){
+    answer[i] = "-";
+}
 var wins = 0;
 var remainingGuesses = 12;
-var wordGenerator = wordBank[Math.floor(Math.random() * wordBank.length)];
-var letter = wordGenerator[0]
+var winsText = document.getElementById("winsText");
+var guessesLeft = document.getElementById("numberGuesses");
+var currentWord = document.getElementById("currentWord");
+currentWord.textContent = answer;
+guessesLeft.textContent = remainingGuesses;
 
 
 document.onkeyup = function(event){  
-
     var userGuess = event.key;
 
-    var wordGenerator = wordBank[Math.floor(Math.random() * wordBank.length)];
+    var wordPicked = wordBank[Math.floor(Math.random() * wordBank.length)];
 
-    if (workingCharacters.indexOf(userGuess) === -1 ){
-       return alert("Enter a valid character")   
-    }
+    while (remainingGuesses > 0){
+
+    
+
+        
+        var triedCharacters = [];
+        var correctCharacters =[];
+
+       
+              
+        if (workingCharacters.indexOf(userGuess) === -1 ){
+            return alert("Enter a valid character");   
+        }
+
+
+        for(var i = 0; i < wordPicked.length; i++){
+            if (wordPicked[i] === userGuess){
+                answer[i] = userGuess;
+                currentWord.textContent = answer
+            }
+
+        }
+        
+
 
     
 
 
+    }
 }
 
-console.log(wordGenerator);
-console.log(letter)
+console.log(wordPicked);
+console.log(answer)
+console.log(remainingGuesses)
