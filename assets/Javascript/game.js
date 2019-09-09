d3d7392c6e96bf8130b19f318a2316e15a4e32d2
KailerS <KailerS@users.noreply.github.com>
2019-09-09 09:45:43 -0600

    Changed some of the loops in javascript

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -12,42 +12,39 @@ var guessesLeft = document.getElementById("numberGuesses");
 var currentWord = document.getElementById("currentWord");
 currentWord.textContent = answer;
 guessesLeft.textContent = remainingGuesses;
+var triedCharacters = [];
+var badCharacters = document.getElementById("lettersGuessed")
 
 
 document.onkeyup = function(event){  
     var userGuess = event.key;
 
     var wordPicked = wordBank[Math.floor(Math.random() * wordBank.length)];
-
-    while (remainingGuesses > 0){
-
-    
-
-        
-        var triedCharacters = [];
-        var correctCharacters =[];
-
-       
+      
               
-        if (workingCharacters.indexOf(userGuess) === -1 ){
-            return alert("Enter a valid character");   
-        }
+    if (workingCharacters.indexOf(userGuess) === -1 ){
+        return alert("Enter a valid character");   
+    }
+
+    if (remainingGuesses === 0){
+        alert("Game Over! You ran out of guesses.")
+    }
 
 
-        for(var i = 0; i < wordPicked.length; i++){
-            if (wordPicked[i] === userGuess){
+    for(var i = 0; i < wordPicked.length; i++){
+        if (wordPicked[i] === userGuess){
                 answer[i] = userGuess;
                 currentWord.textContent = answer
-            }
-
         }
+
+    }
         
 
 
     
 
 
-    }
+    
 }
 
 console.log(wordPicked);
