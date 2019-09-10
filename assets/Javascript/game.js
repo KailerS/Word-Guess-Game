6ea1ccc2d9c2cf7614639d72e36c8cc9d194e960
KailerS <KailerS@users.noreply.github.com>
2019-09-09 20:07:49 -0600

    game is working adding some more touches

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -9,67 +9,72 @@ for (var i = 0; i < wordPicked.length; i++){
 var wins = 0;
 var remainingGuesses = 12;
 var winsText = document.getElementById("winsText");
-// var guessesLeft = document.getElementById("numberGuesses");
 var currentWord = document.getElementById("currentWord");
 currentWord.textContent = answer;
-// guessesLeft.textContent = remainingGuesses;
 var triedCharacters = [];
 var badCharacters = document.getElementById("lettersGuessed");
 badCharacters.textContent = triedCharacters;
+var guessesLeft = document.getElementById("numberGuesses");
+guessesLeft.textContent = remainingGuesses;
+
+
+
 
 
 document.onkeyup = function(event){  
     var userGuess = event.key;
 
-    var guessesLeft = document.getElementById("numberGuesses");
-    guessesLeft.textContent = remainingGuesses;
-
     function gameReset(){
-        wordPicked;
         triedCharacters = [];
         remainingGuesses = 12;
-    }
-
-
-
-
-      
-              
+        wordPicked = wordBank[Math.floor(Math.random() * wordBank.length)];
+        word = wordPicked.split("");
+        answer = [];
+        for (var i = 0; i < wordPicked.length; i++){
+            answer[i] = "-";
+        }
+        currentWord.textContent = answer;
+        guessesLeft.textContent = remainingGuesses;
+        badCharacters.textContent = triedCharacters;
+    }      
+    
     if (workingCharacters.indexOf(userGuess) === -1 ){
         return alert("Enter a valid character");   
     }
-
-    if (remainingGuesses === 0){
-        gameReset()
-        alert("Game Over! You ran out of guesses.")
-    }
-
-
+    
+    
     for(var i = 0; i < word.length; i++){
         if (word[i] === userGuess){
-                answer[i] = userGuess;
-                currentWord.textContent = answer;
+            answer[i] = userGuess;
+            currentWord.textContent = answer;
+            
         }           
-    }
-        
-
-
+    }   
+    
     if (word.indexOf(userGuess) === -1){
-        -- remainingGuesses;
         triedCharacters.push(userGuess);
         badCharacters.textContent = triedCharacters;
+        -- remainingGuesses;
+        guessesLeft.textContent = remainingGuesses;
+    
+    } 
+    
+    
+    if (remainingGuesses === 0){
+        gameReset();
+        alert("Game Over! You ran out of guesses.");
     }
 
 
-        
-
-
-    
-
+    if (answer.join("") === wordPicked){
+        alert("Congratulations you've won!")
+        currentWord.textContent = answer;
+        // gameReset()
+    }
 
     
 }
 
 console.log(wordPicked);
-console.log(answer)
-console.log(remainingGuesses)
+console.log(answer);
+console.log(word);
