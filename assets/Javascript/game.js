14204124ea35a9c663e9c1c8137934c79a3964e6
KailerS <KailerS@users.noreply.github.com>
2019-09-12 19:28:02 -0600

    used Mike's advice and used .join to get rid of commas

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -10,7 +10,7 @@ var wins = 0;
 var remainingGuesses = 12;
 var winsText = document.getElementById("winsText");
 var currentWord = document.getElementById("currentWord");
-currentWord.textContent = answer;
+currentWord.textContent = answer.join("");
 var triedCharacters = [];
 var badCharacters = document.getElementById("lettersGuessed");
 badCharacters.textContent = triedCharacters;
@@ -32,7 +32,7 @@ document.onkeyup = function(event){
         for (var i = 0; i < wordPicked.length; i++){
             answer[i] = "-";
         }
-        currentWord.textContent = answer;
+        currentWord.textContent = answer.join("");
         guessesLeft.textContent = remainingGuesses;
         badCharacters.textContent = triedCharacters;
     }      
@@ -45,7 +45,7 @@ document.onkeyup = function(event){
     for(var i = 0; i < word.length; i++){
         if (word[i] === userGuess){
             answer[i] = userGuess;
-            currentWord.textContent = answer;
+            currentWord.textContent = answer.join("");
             
         }           
     }   
@@ -67,7 +67,7 @@ document.onkeyup = function(event){
 
     if (answer.join("") === wordPicked){
         alert("Congratulations you've won!")
-        currentWord.textContent = answer;
+        currentWord.textContent = answer.join("");
         wins ++;
         winsText.textContent = "Wins: " + wins;
         displayName.textContent = "Last player guessed correctly: " + wordPicked;  
