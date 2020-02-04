9e7ed4e084d6a205656553d8c57a635617053ee0
KailerS <KailerS@users.noreply.github.com>
2020-02-04 14:07:24 -0700

    Working on adding comments for clarity

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,22 +1,33 @@
+// The list of players that can be randomly selected
 const wordBank = ["trout", "bellinger", "yelich", "pujols", "haniger", "goldschmidt", "molina"];
+// Catch to make sure the user types a letter to guess
 const workingCharacters = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+// Picks the word from the bank
 let wordPicked = wordBank[Math.floor(Math.random() * wordBank.length)];
+// Splits the chosen word into individual letters
 let word = wordPicked.split("");
 let answer = [];
 for (let i = 0; i < wordPicked.length; i++){
     answer[i] = "-";
 }
+// Win counter
 let wins = 0;
+// Guesses left
 let remainingGuesses = 12;
+// Grabbing and storing values with their jQuery selector
 const winsText = document.getElementById("winsText");
 const currentWord = document.getElementById("currentWord");
-currentWord.textContent = answer.join("");
-let triedCharacters = [];
 const badCharacters = document.getElementById("lettersGuessed");
-badCharacters.textContent = triedCharacters;
-let guessesLeft = document.getElementById("numberGuesses");
-guessesLeft.textContent = remainingGuesses;
 let displayName = document.getElementById("playerName")
+let guessesLeft = document.getElementById("numberGuesses");
+// Letters that have been guessd
+let triedCharacters = [];
+// Bring the word together on the spaces
+currentWord.textContent = answer.join("");
+// Print the letters that were not part of the word but guesses
+badCharacters.textContent = triedCharacters;
+// Print out the remaining guesses
+guessesLeft.textContent = remainingGuesses;
 
 
 
